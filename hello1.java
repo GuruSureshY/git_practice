4fa5fd52d6a06cb46d883a3b567a6ef71745fff1
Guru Suresh <gurusureshy7036@gmail.com>
2024-04-30 22:24:53 +0530

    hi it 10:30.

---
+++ hello1.java
@@ -0,0 +1,10 @@
+package git_practice;
+class hello1{
+
+    public static void main(String []a){
+        byte a=10;
+        byte b=30;
+        int c =(byte)a*b;
+        System.out.println(c);
+    }
+}
